bfb49e30a24d77484333679b9d63fe401795ce02
cmac88 <cmclaughlin88@gmail.com>
2018-08-31 12:42:05 -0700

    fixed src bug

---
+++ src/main/java/com/codingdojo/photog2/models/Image.java
@@ -0,0 +1,15 @@
+package com.codingdojo.photog2.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="images")
+public class Image {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+}
